e0b71a5384d55f1e2f3a33fea72fe3914f11b174
Vladislav Smirnov <Smrnv.Dev99@gmail.com>
2024-01-12 07:13:24 +0300

    Task_02

---
+++ Lesson_01/Task_02/Program.cs
@@ -0,0 +1,18 @@
+﻿int a = 5;
+int b = 6;
+int sum = a + b;
+Console.WriteLine(sum);
+
+long c = 6;
+long sum_2 = c + a;
+Console.WriteLine(sum_2);
+
+double d = 5.5;
+double sum_3 = d + a;
+
+int product1 = a * b;
+long product2 = c * a;
+double product3 = a * d;
+Console.WriteLine(product1);
+Console.WriteLine(product2);
+Console.WriteLine(product3);
