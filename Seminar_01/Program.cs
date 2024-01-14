6e5963c897631bb30f1f885d53f7a7512bd1d707
Vladislav Smirnov <Smrnv.Dev99@gmail.com>
2024-01-14 10:08:06 +0300

    02

--- Seminar_01/Program.cs
+++ Seminar_01/Program.cs
@@ -23,17 +23,36 @@
 // Console.WriteLine(input + 1000);
 
 
-Console.Write("Введите певрое число: ");
-int firstNumber = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите первое число: ");
+//int firstNumber = Convert.ToInt32(Console.ReadLine());
 
-Console.Write("Введите второе число: ");
-int secondNumber = Convert.ToInt32(Console.ReadLine()); 
+//Console.Write("Введите второе число: ");
+//int secondNumber = Convert.ToInt32(Console.ReadLine());
 
-if(firstNumber == secondNumber * secondNumber)
+//if (firstNumber == secondNumber * secondNumber)
+//{
+//    Console.WriteLine("Да, " + firstNumber + " является квадратом от числа " + secondNumber);
+//}
+
+//else //a не является квадратом от числа b
+//{
+//    Console.WriteLine("Нет, " + firstNumber + " не является квадратом от числа " + secondNumber);
+//}
+
+
+// Напишите программу, которая на вход принимает
+// целое число N, а на выходе показывает все целые 
+// целые числа в промежутке от -N до N.
+// Примеры:
+// 4 => -4, -3, -2, -1, 0, 1, 2, 3, 4
+// 2 => -2, -1, 0, 1, 2
+
+Console.Write("Введите число N:");
+int N = Convert.ToInt32(Console.ReadLine());
+int negativeN = N * (-1);
+
+while (negativeN <= N)
 {
-    Console.WriteLine("Да," + firstNumber + "является квадратом от числа " + secondNumber);
-}
-else //a не является квадратом от числа b
-{
- Console.WriteLine("Нет," + firstNumber + "является квадратом от числа " + secondNumber);
+    Console.Write(negativeN + " ");
+    negativeN++;
 }
